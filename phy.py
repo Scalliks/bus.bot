57dde70f5857c92236b34fad8c1756110869b5eb
Scalliks <egorfedoorov@mail.ru>
2025-08-22 16:37:58 +1100

    Фикс фикса фиксd

--- phy.py
+++ phy.py
@@ -246,8 +246,14 @@ app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler)
 
 import asyncio
 if __name__=='__main__':
-    asyncio.run(app.run_webhook(
-    listen='0.0.0.0',
-    port=PORT,
-    webhook_url=WEBHOOK_URL
-))
+
+    async def main():
+        await app.start()
+        await app.updater.start_webhook(
+            listen='0.0.0.0',
+            port=PORT,
+            webhook_url=WEBHOOK_URL
+        )
+        print(f'Bot is running on {WEBHOOK_URL}')
+        await app.updater.idle()
+    asyncio.run(main())
